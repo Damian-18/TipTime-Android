a85cd9fce216547a93a714587785ab47f3409dff
Damian-18 <s23120229@alumnos.itsur.edu.mx>
2026-09-13 18:08:48 -0600

    Implementar selección de porcentaje de propina y opción de redondeo

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -42,13 +43,12 @@ import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
 
-
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.input.KeyboardType
-
+import androidx.compose.ui.text.input.ImeAction
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
@@ -68,9 +68,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeLayout() {
     var amountInput by remember { mutableStateOf("") }
-
+    var tipInput by remember { mutableStateOf("") }
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+    val tip = calculateTip(amount,tipPercent)
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -85,11 +86,26 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField(value = amountInput,
+        EditNumberField(label = R.string.bill_amount,
+            keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Next
+            ),
+            value = amountInput,
             onValueChange = { amountInput = it },
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth())
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            value = tipInput,
+            onValueChange = { tipInput=it},
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
+        )
         Text(
             text = stringResource(R.string.tip_amount,tip),
             style = MaterialTheme.typography.displaySmall
@@ -103,7 +119,7 @@ fun TipTimeLayout() {
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+private fun calculateTip(amount: Double, tipPercent: Double): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
 }
@@ -117,15 +133,16 @@ fun TipTimeLayoutPreview() {
 }
 
 @Composable
-fun EditNumberField(value: String,
+fun EditNumberField(@StringRes label: Int, keyboardOptions: KeyboardOptions,
+                    value: String,
                     onValueChange: (String) -> Unit,
                     modifier: Modifier = Modifier) {
     TextField(
         value = value,
         onValueChange = onValueChange,
         singleLine = true,
-        label= {Text(stringResource(R.string.bill_amount))},
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        label= {Text(stringResource(label))},
+        keyboardOptions = keyboardOptions,
         modifier = modifier
     )
 }
